64e4646a5f7d01a21a7f186da4e45078e587d9c8
PedroHenriqueph7 <pedrohenrizzx7@gmail.com>
2026-01-08 09:50:25 -0300

    feat: Menu Hamburguer Responsivo

--- js/script.js
+++ js/script.js
@@ -7,3 +7,44 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        // Toggle: Se tem a classe tira, se não tem coloca
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", this.handleClick);
+        
+        // Opcional: Fechar o menu ao clicar em um link
+        this.navLinks.forEach((link) => {
+            link.addEventListener("click", () => this.handleClick());
+        });
+    }
+
+    init() {
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+// Inicializa o menu
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    "#lista_menus",
+    "#lista_menus li"
+);
+mobileNavbar.init();
